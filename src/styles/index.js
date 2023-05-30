03b8dbb343392e7fb4d8fa2d551b5504bcc02d84
daikagoku <80370767+daikagoku@users.noreply.github.com>
2023-05-30 20:52:56 +0700

    update styles

---
+++ src/styles/index.js
@@ -0,0 +1,13 @@
+import { memo, Fragment, useEffect } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import themes from './Theme.module.css';
+library.add(fas, far);
+function StylesComponent({ children }) {
+  useEffect(() => {
+    document.body.classList.add(themes.default, themes.light);
+  }, []);
+  return <Fragment>{children}</Fragment>;
+}
+export default memo(StylesComponent);
